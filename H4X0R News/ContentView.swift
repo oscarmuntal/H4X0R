dc787880fa1caa2e4261717e8e4fcfea4756d842
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-01 14:02:53 +0200

    Simple initial List embedded in a NavigationView

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("Hello, world!")
+                Text("Good by world!")
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+            
     }
 }
 
