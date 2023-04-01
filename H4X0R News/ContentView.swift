2eb59fa67ff3aaabc09907d6ffec21a72a17d683
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-01 17:04:16 +0200

    Showing the main list of articles from the Hacker News API: Calling the fetchData NetworkManager method from the onAppear (viewDidLoad) method when the ContentView starts and binding it to the List using the Observable Pattern.

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -8,23 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
+            List(networkManager.posts) { post in
                 Text(post.title)
             }
             .navigationBarTitle("H4X0R NEWS")
         }
-            
+        .onAppear {
+            self.networkManager.fetchData()
+        }
     }
 }
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Hola")
-]
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
