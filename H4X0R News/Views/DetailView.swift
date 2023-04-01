2473b416ef081588973016941b61583b448e8ec4
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-01 17:49:00 +0200

    WebView for SwiftUI using the UIKit library WebKit

--- H4X0R News/Views/DetailView.swift
+++ H4X0R News/Views/DetailView.swift
@@ -6,17 +6,32 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
     let url: String?
-    
     var body: some View {
-        Text("Hello, World!")
+        WebView(urlString: url)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let safeString = urlString,
+              let url = URL(string: safeString) else { return }
+        let request = URLRequest(url: url)
+        uiView.load(request)
     }
 }
